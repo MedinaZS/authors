031692d6c58a2867e0d0fe46f1e7e75f5ed39d9d
MedinaZS <101607623+MedinaZS@users.noreply.github.com>
2023-01-24 03:25:09 -0300

    first commit

---
+++ client/src/views/Main.js
@@ -0,0 +1,40 @@
+import React, { useEffect, useState } from 'react'
+import axios from 'axios'
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
+import AuthorList from '../components/AuthorList'
+import Update from './Update'
+import AuthorForm from '../components/AuthorForm'
+
+const Main = () => {
+
+    const [errors, setErrors] = useState({});
+
+
+    const create = (serie) => {
+        axios.post('http://localhost:8000/api/author/new', serie)
+            .then(res => {
+                //Para que entre aca, en el controlador debe de estar res.json
+                console.log("Created succesfully", res);
+                //Vaciar errores
+                setErrors({});
+            })
+            .catch(err => {
+                console.log("Error creating", err);
+                setErrors(err.response.data.errors);
+            });
+    }
+
+    return (
+        <div style={{ width: '400px' }}>
+            <BrowserRouter>
+                <Routes>
+                    <Route path='/' element={<AuthorList />} />
+                    <Route path='/new' element={<AuthorForm type={"create"} onSubmitProp={create} errors={errors} initialName={""} />} />
+                    <Route path='/edit/:id' element={<Update />} />
+                </Routes>
+            </BrowserRouter>
+        </div>
+    )
+}
+
+export default Main
